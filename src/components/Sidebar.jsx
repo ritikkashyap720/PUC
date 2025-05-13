e3dc2986b8e0332cc3644c75acf4c3d008cc5df1
ritik <ritikkashyap7404@gmail.com>
2025-05-13 17:57:31 +0530

    done sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 
 function Sidebar() {
   return (
-    <div>
-      Sidebar
+    <div className="w-[500px] min-h-screen bg-[#1C1B2A] text-white p-6 flex items-center justify-center">
+      <div>
+        <h2 className="text-4xl font-bold mb-10">Azure Cost Dashboard</h2>
+        <p className="text-xl font-semibold mb-2 ">Dashboard Features</p>
+        <div className="flex flex-col gap-4 text-xl font-semibold">
+          <button
+            className="w-full flex items-center justify-center gap-2 py-3 border rounded-[10px] border-gray-600 hover:bg-gray-700 cursor-pointer">
+            📊 <span>Main Dashboard</span>
+          </button>
+
+          <button className="w-full flex items-center justify-center gap-2 py-3 border rounded-[10px] border-gray-600  hover:bg-gray-700 cursor-pointer transition">
+            💰 <span>Implemented Savings</span>
+          </button>
+
+          <button className="w-full flex items-center justify-center gap-2 py-3 border rounded-[10px] border-gray-600  hover:bg-gray-700 cursor-pointer transition">
+            🔔 <span>Budget Alerts</span>
+          </button>
+
+          <button className="w-full flex items-center justify-center gap-2 py-3 border rounded-[10px] border-gray-600  hover:bg-gray-700 cursor-pointer transition">
+            📉 <span>Cost Details</span>
+          </button>
+        </div>
+      </div>
     </div>
+
   )
 }
 
